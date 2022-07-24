ded318c6cb4e8cf48f747cadb93719b2f0215532
James Brumond <james@jbrumond.me>
2022-07-24 16:55:58 -0700

    experimenting with github actions publishing

--- scripts/publish-setup.js
+++ scripts/publish-setup.js
@@ -3,7 +3,9 @@ const { resolve } = require('path');
 const { version } = require('../package.json');
 const { writeFileSync } = require('fs');
 
+const [ prefix ] = process.argv.slice(2);
 const tags_file = resolve(__dirname, '../.tags');
+const tag_prefix = prefix ? `${prefix}:` : '';
 
 const [ base_version, extra ] = version.split('-');
 const [ major, minor, patch ] = base_version.split('.');
@@ -12,20 +14,19 @@ const [ pre_release_type ] = pre_release.split('.');
 
 const tags
 	= pre_release ? [
-		`${major}-${pre_release_type}`,                      // e.g. "1-alpha"
-		`${major}.${minor}-${pre_release_type}`,             // e.g. "1.2-alpha"
-		`${major}.${minor}.${patch}-${pre_release_type}`,    // e.g. "1.2.3-alpha"
+		`${tag_prefix}${major}-${pre_release_type}`,                      // e.g. "1-alpha"
+		`${tag_prefix}${major}.${minor}-${pre_release_type}`,             // e.g. "1.2-alpha"
+		`${tag_prefix}${major}.${minor}.${patch}-${pre_release_type}`,    // e.g. "1.2.3-alpha"
 	]
 	: [
-		`${major}`,                                          // e.g. "1"
-		`${major}.${minor}`,                                 // e.g. "1.2"
-		`${major}.${minor}.${patch}`,                        // e.g. "1.2.3"
+		`${tag_prefix}${major}`,                                          // e.g. "1"
+		`${tag_prefix}${major}.${minor}`,                                 // e.g. "1.2"
+		`${tag_prefix}${major}.${minor}.${patch}`,                        // e.g. "1.2.3"
 	];
 
 if (pre_release && pre_release !== pre_release_type) {
-	tags.push(`${major}.${minor}.${patch}-${pre_release}`);  // e.g. "1.2.3-alpha.4"
+	tags.push(`${tag_prefix}${major}.${minor}.${patch}-${pre_release}`);  // e.g. "1.2.3-alpha.4"
 }
 
 writeFileSync(tags_file, tags.join(','), 'utf8');
-
 console.log(`Wrote ".tags" file with the following container image tags:\n- ${tags.join('\n- ')}`);
